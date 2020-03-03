1a2e0312c24a8b4d492b126d285e0da31c1136ce
Abi Oluwade <abi.oluwade1@gmail.com>
2020-03-03 18:40:19 +0000

    working azure pipeline

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,6 @@ COPY package.json ./
 
 RUN npm install
 
-ENV DB_HOST=mongodb://mongo:27017/posts
-
 COPY . .
 
 EXPOSE 3000
--- dockerprov.sh
+++ dockerprov.sh
@@ -1,4 +1,8 @@
-sudo apt-get updat
+sudo apt-get update
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo chown jenkinsslave:jenkinsslave /home/jenkinsslave/.docker -R
+sudo chmod g+rwx "/home/jenkinsslave/.docker" -R
+sudo chmod 777 /var/run/docker.sock
+sudo apt install default-jre
